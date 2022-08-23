efcfc9dd23f2539d972d2f48652ae02182a1b13a
William T. Le <letw@posteo.net>
2022-08-23 13:50:33 -0400

    don't pass pytest args to mypy and flake8

--- noxfile.py
+++ noxfile.py
@@ -10,17 +10,15 @@ nox.options.sessions = "lint", "type", "test"
 @nox.session(python=python)
 def lint(session):
     """Check the codebase against the Flake8 Google-style guide."""
-    args = session.posargs or locations
     session.run("poetry", "install", external=True)
-    session.run("flake8", *args)
+    session.run("flake8", *locations)
 
 
 @nox.session(python=python)
 def type(session):
     """Typecheck the codebase using MyPy."""
-    args = session.posargs or locations
     session.run("poetry", "install", external=True)
-    session.run("mypy", *args)
+    session.run("mypy", *locations)
 
 
 @nox.session(python=python)
