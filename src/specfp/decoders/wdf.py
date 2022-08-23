9adec6de804fc1f4815540734578beab57943cb6
William T. Le <letw@posteo.net>
2022-08-23 12:38:52 -0400

    decode an entire WDF file using a FSM

--- src/specfp/decoders/wdf.py
+++ src/specfp/decoders/wdf.py
@@ -2,10 +2,15 @@
 
 from __future__ import annotations
 from construct import this
+from typing import Callable
 from .. import converters as to
 
-import enum
+import attrs
+import collections
 import construct
+import enum
+import fsspec
+import transitions
 
 
 Unused = construct.Computed(construct.Byte[this.header.size - this._sizing])
@@ -149,3 +154,88 @@ class Block(enum.Enum):
     CHLG = Default(b"CHLG") * "Changelog"
     SURF = Default(b"SURF") * "Surface"
     PSET = Default(b"PSET") * "Stream is Pset"
+
+
+@attrs.define(slots=False)
+class WDF:
+    """A WDF binary file format reader.
+
+    WDF files contain (Raman) spectroscopy data in the form of one or more
+    spectrum consisting of N different points per acquisition. The binary file
+    contains a sequence of blocks, each of which is composed of a header and
+    its payload.
+
+    Args:
+        urlpath: the WDF local or remote file path.
+
+    Methods:
+        decode: decode all of the detected WDF blocks.
+        close: close the opened WDF bitstream.
+
+    Attributes:
+        stream: the currently opened bitstream.
+        blocks: an ordered mapping of block name to decoded data.
+        decoders: an ordered mapping of block name to the decoding construct.
+        states: the available decoding states (registered block decoders).
+
+    Examples:
+        >>> decoder = WDF('tests/WDF/single.wdf')
+        >>> decoder.decode()
+        True
+        >>> list(decoder.blocks.keys())[:2]
+        ['WDF1', 'DATA']
+    """
+
+    urlpath: str
+
+    def __attrs_pre_init__(self):
+        """Initialize the Finite State Machine controller."""
+        self.machine = transitions.Machine(
+            states=[block.name for block in Block],
+            auto_transitions=False,
+            queued=True,
+            model=self)
+
+    def __attrs_post_init__(self):
+        """Open the WDF bitstream and configure the state transitions."""
+        self.blocks = collections.OrderedDict()
+        self.decoders = collections.OrderedDict()
+        self.stream = fsspec.open(self.urlpath, mode="rb").open()
+        self.close = self.stream.close
+        for block in Block:
+            self.machine.add_transition(
+                "decode",
+                prepare="_identify",
+                conditions=self.identified(block.name),
+                source=list(self.machine.states),
+                dest=block.name,
+                after="decode")
+            getattr(self.machine, f"on_enter_{block.name}")("_decode")
+
+    def identified(self, block: str) -> Callable[[], bool]:
+        """Return a thunk that checks what the next block is to be decoded.
+
+        This utility method is useful to call once the next block has been
+        identified but the current reader's state still hasn't transitioned
+        yet. Once the transition occurs, the thunk is equivalent to the
+        matching "is_<state>" method that already exists since the current
+        state and the most redecently decoded block are the same.
+        """
+        def is_block() -> bool:
+            """Check if the next block matches the closure variable "block"."""
+            return next(reversed(self.blocks)) == block
+        return is_block
+
+    def _identify(self):
+        """Peek at the next 4 bytes of the stream to identify the block."""
+        magic = construct.Peek(Header().magic).parse_stream(self.stream)
+        self.blocks[magic] = None
+
+    def _decode(self):
+        """Decode a single WDF block's header and payload from the stream."""
+        self.decoders[self.state] = decoder = Block[self.state].value
+        if self.state in ("XLST", "YLST"):
+            self.blocks[self.state] = decoder.parse_stream(
+                self.stream, size=self.blocks["WDF1"][self.state])
+        else:
+            self.blocks[self.state] = decoder.parse_stream(self.stream)
