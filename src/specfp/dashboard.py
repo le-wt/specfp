9962342a6cac23da322accf702d9713106016c2a
William T. Le <letw@posteo.net>
2022-09-09 12:36:50 -0400

    read WDF and plot resulting spectra

--- src/specfp/dashboard.py
+++ src/specfp/dashboard.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 from dash import Dash, dcc, html, Input, Output, State
+from . import __name__, decoders, raman_tools as raman
 
 import base64
 import io
@@ -61,12 +62,17 @@ nav = html.Section([
         "Load spectra from selected files",
         id="preprocessing-transform")])
 
+figure = html.Div(id="spectra", children=[
+    dcc.Graph(id="spectra-graph"),
+    dcc.Dropdown(id="spectra-bands", value=[], persistence=True, multi=True),
+])
+
 
 # Dashboard HTML
 app.layout = html.Main([
     header,
     nav,
-    dcc.Graph(id="spectra-graph"),
+    figure,
 ])
 
 
@@ -79,3 +85,75 @@ def select_spectra(contents, filenames):
         db.hmset("spectrum", dict(zip(filenames, contents)))
     options = sorted(filename.decode("ascii") for filename in db.hkeys("spectrum"))
     return options
+
+
+@app.callback(
+        Output("cache", "data"),
+        Input("preprocessing-transform", "n_clicks"),
+        State("files-dropdown", "value"),
+        State("preprocessing-checklist", "value"),
+        State("cache", "data"))
+def load_spectra(_, filenames, preprocessing, cache):
+    if not filenames:
+        return {}
+    data = db.hgetall("spectrum")
+    spectra = {}
+    for filename, content in data.items():
+        filename = filename.decode("ascii")
+        if not filename in filenames:
+            continue
+        _, content = content.split(b",")
+        stream = io.BytesIO(base64.b64decode(content))
+        try:
+            acquisitions = decoders.load(stream, verbose=0)
+        except AttributeError:
+            continue
+        else:
+            spectra[filename] = acquisitions.mean(axis=0)
+    df = discretize(pd.DataFrame(spectra).sort_index())
+    df.index.name = "wavelength"
+    if "Cosmic Ray Removal" in preprocessing:
+        df = df.apply(raman.cosmic_rays_removal, raw=True)
+    if "Savgol" in preprocessing:
+        df = df.apply(
+                raman.savgol_filter,
+                raw=True,
+                window_length=11,
+                polyorder=3)
+    if "Raman" in preprocessing:
+        df = df.apply(raman.bubblefill, raw=True)
+    if "SNV" in preprocessing:
+        df = df.apply(raman.SNV, raw=True)
+    cache["spectra"] = str(df.to_json())
+    return cache
+
+
+@app.callback(
+        Output("spectra-graph", "figure"),
+        Input("cache", "data"))
+def plot_spectra(cache):
+    df = pd.read_json(cache["spectra"])
+    return plot(df)
+
+
+def discretize(df: pd.DataFrame, copy: bool = False) -> pd.DataFrame:
+    """Discretize the index of a dataframe to the closest integral value."""
+    if copy:
+        df = df.copy()
+    df.index = np.round(df.index).astype(int)
+    df = df.loc[~df.index.duplicated()]
+    df = df.reindex(range(df.index.min(), df.index.max() + 1))
+    df = df.interpolate(method="linear")
+    return df
+
+
+def plot(df):
+    labels={'value': 'Absorption',
+            'wavelength': 'Wavelength (nm)',
+            'variable': 'Acquisition'}
+    fig = px.line(df, labels=labels, hover_name="variable")
+    fig.update_layout(
+            dragmode="select",
+            showlegend=False,
+            hovermode="x unified")
+    return fig
